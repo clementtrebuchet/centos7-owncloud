9436c7446657abf380cf28b7d10ba2dca2957b0b
clement <clement.trebuchet@gmail.com>
2015-04-18 20:28:09 +0200

    correct initApp.sh

--- initApp.sh
+++ initApp.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-
-set -e
+et -e
 
 if [ -f /etc/configured ]; then
         echo '[WARN]Already configured'
 else
+	if [[ ! -d  /etc/nginx/ssl ]]; then
+		mkdir -p /etc/nginx/ssl
+	fi 
         echo "[INFO]Gen AutoSSL cert and key..."
 	openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN" \
 	 -out /etc/nginx/ssl/owncloud.pem -keyout /etc/nginx/ssl/owncloud.key
@@ -14,13 +15,14 @@ else
 	sed -i "s#-x-replace-cert-x-#/etc/nginx/ssl/owncloud.pem#" /etc/nginx_ssl.conf
 	sed -i "s#-x-replace-key-x-#/etc/nginx/ssl/owncloud.key#"  /etc/nginx_ssl.conf
         echo "[INFO]Copy nginx SSL config file"
-	cp /etc/nginx_ssl.conf /etc/nginx/nginx_ssl.conf
+	cp -p  /etc/nginx_ssl.conf /etc/nginx/nginx_ssl.conf
+	ls -lain /etc/nginx/nginx_ssl.conf
 	#to include the domain to the hosts
         echo 127.0.0.1 $CN >> /etc/hosts
         date > /etc/configured
 	
 fi
 echo "[INFO]Starting php-fpm & nginx Services..."
-systemctl enable php-fpm.service && systemctl start  php-fpm.service
-systemctl enable nginx.service && systemctl start nginx.service
+/usr/sbin/php-fpm -F &  /usr/sbin/nginx -c /etc/nginx/nginx_ssl.conf
 echo "[INFO]Go to https://YOUR-HOST/owncloud..."
+
